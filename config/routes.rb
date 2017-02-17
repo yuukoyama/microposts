164b86603e98782d8469a4f9ad52c55ee50c3108
yuukoyama <kym1028kym@gmail.com>
2017-02-17 12:04:41 +0000

    before user_profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   root to: 'static_pages#home'
   get 'signup',  to: 'users#new'
+  get    'login' , to: 'sessions#new'
+  post   'login' , to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   resources :users
 end

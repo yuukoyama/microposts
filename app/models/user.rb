3ddce3b7ac5c65bdb6fec65c52cbe4b602215405
yuukoyama <kym1028kym@gmail.com>
2017-02-20 17:45:53 +0000

    add followings/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,28 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :location, allow_blank: true, 
                       length: { minimum: 2, maximum: 20 }
-  has_many :microposts                    
+  has_many :microposts
+  has_many :following_relationships, class_name:  "Relationship",
+                                     foreign_key: "follower_id",
+                                     dependent:   :destroy
+  has_many :following_users, through: :following_relationships, source: :followed
+  has_many :follower_relationships, class_name:  "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent:   :destroy
+  has_many :follower_users, through: :follower_relationships, source: :follower
+    # 他のユーザーをフォローする
+  def follow(other_user)
+    following_relationships.find_or_create_by(followed_id: other_user.id)
+  end
+
+  # フォローしているユーザーをアンフォローする
+  def unfollow(other_user)
+    following_relationship = following_relationships.find_by(followed_id: other_user.id)
+    following_relationship.destroy if following_relationship
+  end
+
+  # あるユーザーをフォローしているかどうか？
+  def following?(other_user)
+    following_users.include?(other_user)
+  end  
 end
